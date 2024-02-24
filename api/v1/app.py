0cccdc11994db1830ee185861b7e308253d0eba3
owino600 <owinodaniel600@gmail.com>
2024-02-24 18:29:12 +0300

    .\app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Flask Application """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -12,6 +12,14 @@ app.register_blueprint(app_views)
 def close_db(error):
     """Close storage on teardown."""
     storage.close()
+    
+@app.errorhandler(404)
+def not_found(error):
+    """404 errors.
+    responses:
+    404:"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST')
