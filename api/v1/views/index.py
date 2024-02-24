487ce15de13bbd2368015f60111eb11a64de088b
owino600 <owinodaniel600@gmail.com>
2024-02-24 15:04:45 +0300

    .\app.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,7 @@
 from flask import jsonify
 from api.v1.views import app_views
 
-app = Flask(__name__)
 @app_views.route('/status', methods=['GET'])
 def status():
     """Route that returns a JSON status."""
     return jsonify({"status": "OK"})
-app.run(host='0.0.0.0', port=5000)
