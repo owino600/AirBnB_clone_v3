245ce02a546b09d67e484cc5c186bcee97095e49
owino600 <owinodaniel600@gmail.com>
2024-02-24 16:33:53 +0300

    .\app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
+from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def close_storage(exception):
+def close_db(exception):
     storage.close()
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
     app.run(host=host, port=port, threaded=True)
