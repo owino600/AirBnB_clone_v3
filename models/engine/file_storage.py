eb7cc1c405707c53f990c9b6a47912cd56fe3824
owino600 <owinodaniel600@gmail.com>
2024-02-24 14:24:56 +0300

    .\file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+    def get(self, cls, id):
+        """Retrieve one object"""
+        if cls not in classes:
+            return None
+        key = cls + "." + id
+        return self.__objects.get(key, None)
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if cls is None:
+            return len(self.__objects)
+        return len(self.all(cls))
