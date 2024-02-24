7a6247e6de0fbccbf8bd90764d7adeac8b45b395
owino600 <owinodaniel600@gmail.com>
2024-02-24 15:51:15 +0300

    .\index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,23 @@
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Route that returns a JSON status."""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    counts = {}
+    for cls in classes:
+        counts[classes[cls]] = storage.count(cls)
+    return jsonify(counts)
