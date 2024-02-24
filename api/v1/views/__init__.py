b109318b5879860fbce5dcc8f8b16fba35f3de1c
owino600 <owinodaniel600@gmail.com>
2024-02-24 14:51:48 +0300

    .\app.py

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import of everything in the package api.v1.views.index
+# PEP8 will complain about it, but it's normal and this file (v1/views/__init__.py) won't be checked.
+from api.v1.views.index import *
